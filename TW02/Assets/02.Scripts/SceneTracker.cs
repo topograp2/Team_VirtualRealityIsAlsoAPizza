9e8e428b7cd64892c76bda776c1f39040746c455
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-05-29 21:25:21 +0900

    [홍지형] : Status 기능 구현

---
+++ TW02/Assets/02.Scripts/SceneTracker.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// SceneTracker.cs
+public static class SceneTracker
+{
+    public static string previousScene = "";
+    public static int complete = 0; // 달성한 미션 수 (0~5)
+}
+
